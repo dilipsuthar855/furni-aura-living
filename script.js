7c72769fbeabe28efa7bf21180e8a8e787086625
dilipsuthar855 <53169897+dilipsuthar855@users.noreply.github.com>
2026-07-26 05:41:58 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,117 @@
+const faqItems=document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item=>{
+
+const question=item.querySelector(".faq-question");
+
+question.addEventListener("click",()=>{
+
+item.classList.toggle("active");
+
+});
+
+});
+const reveals=document.querySelectorAll(".reveal");
+
+function revealSection(){
+
+reveals.forEach(section=>{
+
+const windowHeight=window.innerHeight;
+
+const revealTop=section.getBoundingClientRect().top;
+
+if(revealTop<windowHeight-120){
+
+section.classList.add("active");
+
+}
+
+});
+
+}
+
+window.addEventListener("scroll",revealSection);
+
+revealSection();
+const navbar=document.querySelector(".navbar");
+
+window.addEventListener("scroll",()=>{
+
+if(window.scrollY>80){
+
+navbar.classList.add("scrolled");
+
+}
+
+else{
+
+navbar.classList.remove("scrolled");
+
+}
+
+});
+const menuToggle = document.querySelector(".menu-toggle");
+const menuIcon = menuToggle.querySelector("i");
+const nav = document.querySelector("nav");
+
+menuToggle.addEventListener("click", () => {
+
+    nav.classList.toggle("active");
+
+    if(nav.classList.contains("active")){
+        menuIcon.classList.remove("fa-bars");
+        menuIcon.classList.add("fa-xmark");
+    }else{
+        menuIcon.classList.remove("fa-xmark");
+        menuIcon.classList.add("fa-bars");
+    }
+
+});
+
+document.querySelectorAll("nav a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        nav.classList.remove("active");
+
+        menuIcon.classList.remove("fa-xmark");
+        menuIcon.classList.add("fa-bars");
+
+    });
+
+});
+const topBtn=document.getElementById("topBtn");
+
+window.addEventListener("scroll",()=>{
+
+if(window.scrollY>500){
+
+topBtn.style.display="block";
+
+}else{
+
+topBtn.style.display="none";
+
+}
+
+});
+
+topBtn.addEventListener("click",()=>{
+
+window.scrollTo({
+
+top:0,
+
+behavior:"smooth"
+
+});
+
+});
+window.addEventListener("load",()=>{
+
+const preloader=document.getElementById("preloader");
+
+preloader.classList.add("hide");
+
+});
